a4c51bc6438beccb535260a81c7abd4d528093ba
dvyas98 <dhimant92@gmail.com>
2020-10-06 14:56:13 -0400

    Assignment1

---
+++ Assets/Cube.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    public string Address;
+    private GameObject _cube;
+    public float X,Y,Z;
+    public float speed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cube = GameObject.Find("NetworkMan");
+    }
+
+    public void deleteCube()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        X = this.transform.position.x;
+        Y = this.transform.position.y;
+        Z = this.transform.position.z;
+        if (_cube.GetComponent<NetworkMan>().myAddress == Address)
+        {
+            _cube.GetComponent<NetworkMan>().cubeX = X;
+            _cube.GetComponent<NetworkMan>().cubeY = Y;
+            _cube.GetComponent<NetworkMan>().cubeZ = Z;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                //Debug.Log("Pressed");
+                transform.Translate(Vector3.forward* Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //Debug.Log("Pressed");
+
+                transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+               // Debug.Log("Pressed");
+
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+               //Debug.Log("Pressed");
+
+                transform.Translate(-Vector3.left * Time.deltaTime * speed);
+            }
+        }
+    }
+}
--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -1,20 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
 
+
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
+    public GameObject playerGO; //Player.
+    public string myAddress;
+    private string defMessage;
+
+    public List<Cube> currentPlayerList;
+    public List<string> newPlayerList;
+    public List<string> droppedPlayerList;
+
+    public float cubeX;
+    public float cubeY;
+    public float cubeZ;
+
+
+
+    private void SetCube()
+    {
+        myAddress = "init";
+        cubeX = 1; cubeY = 1; cubeZ = 1;
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         udp = new UdpClient();
-        
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
+
+        udp.Connect("localhost", 12345); //udp.Connect("3.22.203.136",12345);
 
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
       
@@ -23,47 +45,77 @@ public class NetworkMan : MonoBehaviour
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
         InvokeRepeating("HeartBeat", 1, 1);
+
+        InvokeRepeating("SendPosition", 1, 0.03f);
+
     }
 
     void OnDestroy(){
         udp.Dispose();
     }
 
-
-    public enum commands{
-        NEW_CLIENT,
-        UPDATE
-    };
-    
-    [Serializable]
-    public class Message{
-        public commands cmd;
+    [Serializable] 
+    public class Position
+    {
+        public Vector3 position;
     }
-    
+
     [Serializable]
-    public class Player{
+    public class Player
+    {
         [Serializable]
-        public struct receivedColor{
-            public float R;
-            public float G;
-            public float B;
+        public struct receivedPosition
+        {
+            public float x;
+            public float y;
+            public float z;
         }
         public string id;
-        public receivedColor color;        
+        public receivedPosition position;
+    }
+
+    [Serializable] 
+    public class NewPlayer
+    {
+        public Player player;     //New Player Class.
     }
 
     [Serializable]
-    public class NewPlayer{
-        
+    public class ListOfPlayers
+    {
+        public Player[] players;
+
     }
 
     [Serializable]
-    public class GameState{
+    public class ListOfdroppedPlayerList
+    {
+        public string address;
+    }
+
+    public enum commands{
+        NEW_CLIENT,  //0
+        UPDATE,       //1
+        PLAYER_DISCONNECTED,//2
+        LIST_OF_PLAYERS,//3
+    };
+
+    [Serializable]
+    public class Message
+    {
+        public commands cmd;
+    }
+
+
+    [Serializable]
+    public class GameState
+    {
         public Player[] players;
     }
 
     public Message latestMessage;
-    public GameState lastestGameState;
+    public GameState ActiveGameState;
+
     void OnReceived(IAsyncResult result){
         // this is what had been passed into BeginReceive as the second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
@@ -76,18 +128,50 @@ public class NetworkMan : MonoBehaviour
         
         // do what you'd like with `message` here:
         string returnData = Encoding.ASCII.GetString(message);
+
         Debug.Log("Got this: " + returnData);
-        
-        latestMessage = JsonUtility.FromJson<Message>(returnData);
+        defMessage = "Got this: " + returnData;
+
+
+        latestMessage= JsonUtility.FromJson<Message>(returnData);
+
         try{
-            switch(latestMessage.cmd){
+            switch (latestMessage.cmd)
+            {
                 case commands.NEW_CLIENT:
+                    Debug.Log("New Player");
+                    Debug.Log(defMessage);
+                    NewPlayer p = JsonUtility.FromJson<NewPlayer>(returnData);
+                    if (myAddress == "init")
+                    {
+                        myAddress = p.player.id;
+                    }
+                    newPlayerList.Add(p.player.id); //Spawn Player with this ID.
                     break;
+
                 case commands.UPDATE:
-                    lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    ActiveGameState = JsonUtility.FromJson<GameState>(returnData);
+                    break;
+
+                case commands.PLAYER_DISCONNECTED:
+                    ListOfdroppedPlayerList drop = JsonUtility.FromJson<ListOfdroppedPlayerList>(returnData);
+                    Debug.Log("Player Gone.");
+                    Debug.Log(defMessage);
+                    droppedPlayerList.Add(drop.address);
+                    //Drop the player
+                    break;
+                case commands.LIST_OF_PLAYERS:
+                    ListOfPlayers gamePlayers = JsonUtility.FromJson<ListOfPlayers>(returnData);
+                    for (int i = 0; i < gamePlayers.players.Length; i++)
+                    {
+                        if (gamePlayers.players[i].id != myAddress)
+                        {
+                            newPlayerList.Add(gamePlayers.players[i].id);
+                        }
+                    }
                     break;
                 default:
-                    Debug.Log("Error");
+                    Debug.Log("Error: " + returnData);
                     break;
             }
         }
@@ -99,26 +183,77 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
-
+    void SpawnPlayers()
+    {
+        //Spawn the Cube
+        for (int i = 0; i < newPlayerList.Count; i++)
+        {
+            GameObject NewCube = Instantiate(playerGO, Vector3.zero, Quaternion.identity);
+            NewCube.GetComponent<Cube>().Address = newPlayerList[i];
+            currentPlayerList.Add(NewCube.GetComponent<Cube>());
+        }
+        newPlayerList.Clear();
+        newPlayerList.TrimExcess();
     }
 
-    void UpdatePlayers(){
-
+    void UpdatePlayers()
+    {
+        for (int i = 0; i < ActiveGameState.players.Length; i++)
+        {
+            if (ActiveGameState.players[i].id != myAddress)
+            {
+                for (int j = 0; j < currentPlayerList.Count; j++)
+                {
+                    if (currentPlayerList[j].Address == ActiveGameState.players[i].id)
+                    {
+                        float x = ActiveGameState.players[i].position.x;
+                        float y = ActiveGameState.players[i].position.y;
+                        float z = ActiveGameState.players[i].position.z;
+                        currentPlayerList[j].transform.position = new Vector3(x, y, z);
+                    }
+                }
+            }
+        }
     }
 
-    void DestroyPlayers(){
+    void deletePlayers( string address)
+    {
+        for (int i = 0; i < currentPlayerList.Count; i++)
+        {
+            if (currentPlayerList[i].Address == address)
+            {
+                currentPlayerList[i].gameObject.SendMessage("deleteCube");
+            }
+        }
+    }
 
+    void deleteDropped()
+    {
+        for (int i = 0; i < droppedPlayerList.Count; i++)
+        {
+            deletePlayers(droppedPlayerList[i]);
+        }
+        droppedPlayerList.Clear();
+        droppedPlayerList.TrimExcess();
     }
     
     void HeartBeat(){
         Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
         udp.Send(sendBytes, sendBytes.Length);
     }
+    void SendPosition()
+    {
+        Position cube = new Position();
+        cube.position.x = cubeX;
+        cube.position.y = cubeY;
+        cube.position.z = cubeZ;
+        Byte[] sendPosition = Encoding.ASCII.GetBytes(JsonUtility.ToJson(cube));
+        udp.Send(sendPosition, sendPosition.Length);
 
+    }
     void Update(){
         SpawnPlayers();
         UpdatePlayers();
-        DestroyPlayers();
+        deleteDropped();
     }
 }
